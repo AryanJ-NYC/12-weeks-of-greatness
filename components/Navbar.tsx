cdf20adb95f9682522fc116f1b27a1c945b73386
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-21 15:39:31 -0500

    Style Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,32 @@
+import { AppBar, createStyles, Toolbar, Typography, withStyles } from '@material-ui/core';
+import { ClassNameMap } from '@material-ui/core/styles/withStyles';
 import Link from 'next/link';
 
-export default function Navbar() {
+const styles = createStyles({
+  navLink: {
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+});
+
+function Navbar({ classes }: IProps) {
   return (
-    <div>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
-    </div>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar} >
+        <Link href="/signup">
+          <Typography className={classes.navLink} component="a">Signup</Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 }
+
+interface IProps {
+  classes: Partial<ClassNameMap<keyof typeof styles>>;
+}
+
+export default withStyles(styles)(Navbar);
