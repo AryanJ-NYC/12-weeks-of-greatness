1529c76e5990141e3d4a53351179c5a75fe1865c
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-22 12:56:11 -0500

    Extract auth submission to parent class

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,14 +2,8 @@ import AuthComponent, { ISignupForm } from '../components/Auth';
 import firebase from '../lib/firebase';
 
 class LoginPage extends AuthComponent {
-  handleSubmit = async (formState: ISignupForm) => {
-    const { emailAddress: email, password } = formState;
-    try {
-      await firebase.auth().signInWithEmailAndPassword(email, password);
-      this.formApi.reset();
-    } catch (err) {
-      this.setState({ err });
-    }
+  submitToFirebase = async ({ emailAddress: email, password }: ISignupForm) => {
+    return await firebase.auth().signInWithEmailAndPassword(email, password);
   }
 }
 
