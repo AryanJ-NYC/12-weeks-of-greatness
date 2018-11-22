701d4535c6843958e9fcd24de52189801d3a203f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-22 00:50:21 -0500

    Add Snackbar to login screen

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,13 +2,13 @@ import AuthComponent, { ISignupForm } from '../components/Auth';
 import firebase from '../lib/firebase';
 
 class LoginPage extends AuthComponent {
-  async handleSubmit(formState: ISignupForm) {
+  handleSubmit = async (formState: ISignupForm) => {
     const { emailAddress: email, password } = formState;
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
       this.formApi.reset();
     } catch (err) {
-      throw new Error(err);
+      this.setState({ err });
     }
   }
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,27 +1,8 @@
-import { Button, Grid } from '@material-ui/core';
-import AuthComponent from '../components/Auth';
-import MaterialText from '../components/TextField';
+import AuthComponent, { ISignupForm } from '../components/Auth';
 import firebase from '../lib/firebase';
 
-interface ISignupForm {
-  emailAddress: string;
-  password: string;
-}
-
-function FormContent() {
-  return (
-    <Grid justify="center" container>
-      <Grid xs={11} sm={6} lg={4} style={{ display: 'flex', flexDirection: 'column' }} item>
-        <MaterialText label="Email Address" id="email-address" field="emailAddress" type="email" />
-        <MaterialText label="Password" id="password" field="password" type="password" />
-        <Button type="submit">Submit</Button>
-      </Grid>
-    </Grid>
-  );
-}
-
 class SignupPage extends AuthComponent {
-  private handleSubmit = async (formState: ISignupForm) => {
+  handleSubmit = async (formState: ISignupForm) => {
     const { emailAddress: email, password } = formState;
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, password);
