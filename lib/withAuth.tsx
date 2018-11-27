cad0676705ae9705e6ffc1e3c74ce538951f1761
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-27 08:29:08 -0500

    Add Auth HOC

---
+++ lib/withAuth.tsx
@@ -0,0 +1,38 @@
+import { isEmpty } from 'lodash';
+import Router from 'next/router';
+import { Component } from 'react';
+import rebase from '../lib/firebase';
+
+export default function withAuth(AuthComponent) {
+    return class Authenticated extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          isLoading: true,
+          user: {},
+        };
+      }
+
+      componentDidMount() {
+        rebase.initializedApp.auth().onAuthStateChanged(user => {
+          if (!user || isEmpty(user)) {
+            Router.push('/login');
+          } else {
+            this.setState({ user, isLoading: false });
+          }
+        });
+      }
+
+      render() {
+        return (
+          <div>
+          {this.state.isLoading ? (
+              <div>LOADING....</div>
+            ) : (
+              <AuthComponent {...this.props}  user={this.state.user} />
+            )}
+          </div>
+        )
+      }
+    }
+}
