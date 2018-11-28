5e61b5511032b0f48e210d9e0c2508dc656a812f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-27 22:18:36 -0500

    Fix indentation

--- lib/withAuth.tsx
+++ lib/withAuth.tsx
@@ -1,34 +1,35 @@
+import { User } from 'firebase';
 import { isEmpty } from 'lodash';
 import Router from 'next/router';
 import { Component } from 'react';
 import rebase from '../lib/firebase';
 
 export default function withAuth(AuthComponent: React.Component) {
-    return class Authenticated extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          isLoading: true,
-          user: {},
-        };
-      }
+  return class Authenticated extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: true,
+        user: {},
+      };
+    }
 
-      componentDidMount() {
-        rebase.initializedApp.auth().onAuthStateChanged(user => {
-          if (!user || isEmpty(user)) {
-            Router.push('/login');
-          } else {
-            this.setState({ user, isLoading: false });
-          }
-        });
-      }
+    componentDidMount() {
+      rebase.initializedApp.auth().onAuthStateChanged((user: User) => {
+        if (!user || isEmpty(user)) {
+          Router.push('/login');
+        } else {
+          this.setState({ user, isLoading: false });
+        }
+      });
+    }
 
-      render() {
-        return (
-          <div>
-            {this.state.isLoading ? <div>LOADING....</div> : <AuthComponent {...this.props}  user={this.state.user} />}
-          </div>
-        );
-      }
-    };
+    render() {
+      return (
+        <div>
+          {this.state.isLoading ? <div>LOADING....</div> : <AuthComponent {...this.props}  user={this.state.user} />}
+        </div>
+      );
+    }
+  };
 }
